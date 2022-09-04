cb8cabe04b5528d8809a32738c91e8da8defacfd
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2022-09-04 22:38:37 +0530

    Upload

---
+++ src/classCompon.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class classCompon extends React.Component{
+    render(){
+        return <div>
+            <h1>Class Compo</h1>
+        </div>
+    }
+}
+
+export default classCompon;
